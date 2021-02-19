cb45aaf2fa4485f38e1a90068a4b2808f7f57bf4
Wesley <wes.beck21@outlook.com>
2021-02-19 12:05:06 -0800

    commit tree dd59cf7d199a2a5961458bc2ab184c580071b9f5

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  document.getElementById('text').textContent = "This is really cool!"
+}); 
+
